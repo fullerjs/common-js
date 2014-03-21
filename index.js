a7c181bc0c6b28e069d1e911b3d41b20fee481e8
Alexey Novikov <velocityzen@gmail.com>
2014-03-21 15:12:14 +0400

    Updated to latest fuller api

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ var path = require('path');
 
 var Stream = require('./stream');
 
-var CommonJS = function(fuller, plan) {
-	this.options = plan.defaults.stream || {};
+var FILE_ENCODING = 'utf-8';
+
+var CommonJS = function(fuller, options) {
+	this.src = options.src;
+	this.dst = options.dst;
 };
 
-CommonJS.prototype.getStream = function() {
-	return new Stream(this.options);
+CommonJS.prototype.build = function(stream, master) {
+	var next = new Stream();
+
+	if(typeof stream === "string") {
+		var src = path.join(this.src, stream);
+		this.addDependence(src, master);
+		return fs.createReadStream(src, {encoding: FILE_ENCODING}).pipe(next);
+	} else {
+		return stream.pipe(next);
+	}
 };
 
 module.exports = CommonJS;
