4d30b41240f58343f512851454ee0da9f11b8630
Alexey Novikov <velocityzen@gmail.com>
2014-03-21 16:25:32 +0400

    Updated streams

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 var fs = require('fs');
 var path = require('path');
 
-var Stream = require('./stream');
+var prologue = fs.readFileSync(path.join(__dirname, 'js', 'prologue.js'));
+var epilogue = fs.readFileSync(path.join(__dirname, 'js', 'epilogue.js'));
+var itemPrologue = fs.readFileSync(path.join(__dirname, 'js', 'item_prologue.js'));
+var itemEpilogue = fs.readFileSync(path.join(__dirname, 'js', 'item_epilogue.js'));
 
 var FILE_ENCODING = 'utf-8';
 
 var CommonJS = function(fuller, options) {
+	fuller.bind(this);
+	this.Stream = fuller.streams.Wrapper;
+
 	this.src = options.src;
 	this.dst = options.dst;
 };
 
 CommonJS.prototype.build = function(stream, master) {
-	var next = new Stream();
+	var next = new this.Stream({
+		prologue: prologue,
+		epilogue: epilogue,
+		itemPrologue: itemPrologue,
+		itemEpilogue: itemEpilogue
+	});
 
 	if(typeof stream === "string") {
 		var src = path.join(this.src, stream);
