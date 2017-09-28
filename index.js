164903a51876a833888c1b2b919b5165a4225b25
velocityzen <velocityzen@gmail.com>
2017-09-27 22:25:04 -0400

    Updated dependencies

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
-let fs = require('fs');
-let path = require('path');
-let Transform = require('stream').Transform;
-let Material = require('fuller-material-file');
+const fs = require('fs');
+const path = require('path');
+const Transform = require('stream').Transform;
+const Material = require('fuller-material-file');
 
-let prologue = fs.readFileSync(path.join(__dirname, 'js', 'prologue.js')).toString();
-let epilogue = fs.readFileSync(path.join(__dirname, 'js', 'epilogue.js')).toString();
-let prologueManualInit = fs.readFileSync(path.join(__dirname, 'js', 'prologue_manual_init.js')).toString();
-let epilogueManualInit = fs.readFileSync(path.join(__dirname, 'js', 'epilogue_manual_init.js')).toString();
-let itemPrologue = fs.readFileSync(path.join(__dirname, 'js', 'item_prologue.js')).toString();
-let itemEpilogue = fs.readFileSync(path.join(__dirname, 'js', 'item_epilogue.js')).toString();
+const prologue = fs.readFileSync(path.join(__dirname, 'js', 'prologue.js')).toString();
+const epilogue = fs.readFileSync(path.join(__dirname, 'js', 'epilogue.js')).toString();
+const prologueManualInit = fs.readFileSync(path.join(__dirname, 'js', 'prologue_manual_init.js')).toString();
+const epilogueManualInit = fs.readFileSync(path.join(__dirname, 'js', 'epilogue_manual_init.js')).toString();
+const itemPrologue = fs.readFileSync(path.join(__dirname, 'js', 'item_prologue.js')).toString();
+const itemEpilogue = fs.readFileSync(path.join(__dirname, 'js', 'item_epilogue.js')).toString();
 
-let CommonJS = function(fuller, options) {
+const CommonJS = function(fuller, options) {
   fuller.bind(this);
 
   this.src = options.src;
@@ -23,43 +23,37 @@ let CommonJS = function(fuller, options) {
   this.itemEpilogue = options.itemEpilogue || itemEpilogue;
 };
 
-CommonJS.prototype = {
-  build: function(src, dst) {
-    let self = this;
-    let newMat = new Material({
-      id: dst,
-      path: path.join(self.dst, dst)
-    })
-    .error(function(err) {
-      self.error(err);
-    });
+CommonJS.prototype.build = function(src, dst) {
+  const newMat = new Material({
+    id: dst,
+    path: path.join(this.dst, dst)
+  }).error(err => this.error(err));
 
-    let newContent = self.prologue;
+  let newContent = this.prologue;
 
-    if (this.external) {
-      newContent = newContent.replace('<^FILENAME^>', path.basename(dst));
-    }
-
-    let next = new Transform({
-      objectMode: true,
-      transform: function(mat, enc, cb) {
-        mat.getContent(function(content) {
-          newContent += self.itemPrologue;
-          newContent += content.toString();
-          newContent += self.itemEpilogue;
-          cb();
-        });
-      },
-
-      flush: function(done) {
-        newContent += self.epilogue;
-        this.push(newMat.setContent(newContent));
-        done();
-      }
-    });
-
-    return next;
+  if (this.external) {
+    newContent = newContent.replace('<^FILENAME^>', path.basename(dst));
   }
+
+  const next = new Transform({
+    objectMode: true,
+    transform: (mat, enc, cb) => {
+      mat.getContent(content => {
+        newContent += this.itemPrologue;
+        newContent += content.toString();
+        newContent += this.itemEpilogue;
+        cb();
+      });
+    },
+
+    flush: done => {
+      newContent += this.epilogue;
+      next.push(newMat.setContent(newContent));
+      done();
+    }
+  });
+
+  return next;
 };
 
 
